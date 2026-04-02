4c7a71280c7f9dfd35d69f6f5d8e4b353d10cba1
pacho ruiz <alfrrugu@hotmail.com>
2026-04-01 20:30:17 -0600

    adding map to spam tab to indent and deindent

--- lua/plugins/astrocore.lua
+++ lua/plugins/astrocore.lua
@@ -1,5 +1,3 @@
-if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
-
 -- AstroCore provides a central place to modify mappings, vim options, autocommands, and more!
 -- Configuration documentation can be found with `:h astrocore`
 -- NOTE: We highly recommend setting up the Lua Language Server (`:LspInstall lua_ls`)
@@ -79,6 +77,11 @@ return {
 
         -- setting a mapping to false will disable it
         -- ["<C-S>"] = false,
+      },
+
+      v = {
+        ["<Tab>"] = { ">gv", desc = "Indent" },
+        ["<S-Tab>"] = { "<gv", desc = "Unindent" },
       },
     },
   },
